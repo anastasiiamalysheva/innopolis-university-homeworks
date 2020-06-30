861d94249a3aa1b6e0d3cf889c38f20c4d74e3c4
anastasiiamalysheva <malysheva.anastasiia@mail.ru>
2020-06-30 17:13:00 +0300

    added Vending Machine

---
+++ src/ru/malysheva/task7/Drinks.java
@@ -0,0 +1,26 @@
+package ru.malysheva.task7;
+
+public enum Drinks {
+
+    JUICE("Orange juice", 100),
+    WATER("Steel water", 50),
+    TEA("Ice tea", 75);
+
+    private final String name;
+    private final int price;
+
+    private Drinks(String name, int cost) {
+        this.name = name;
+        this.price = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+}
+
---
+++ src/ru/malysheva/task7/VendingMachine.java
@@ -0,0 +1,43 @@
+package ru.malysheva.task7;
+
+import java.util.Scanner;
+
+public class VendingMachine {
+    public static void main(String[] args) {
+        for (Drinks d : Drinks.values()) {
+            System.out.println(d);
+        }
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please choose your drink: ");
+        int choice = in.nextInt();
+
+        System.out.println("Insert money: ");
+        int money = in.nextInt();
+
+
+        if (choice == 1) {
+            System.out.println("Your balance now: " + (money - 100));
+
+        } else if (choice == 2) {
+            System.out.println("Your balance now: " + (money - 50));
+
+        } else if (choice == 3) {
+            System.out.println("Your balance now: " + (money - 75));
+
+        } else {
+            System.out.println("Your choice: " + choice + " is not available. Please choose another one.");
+        }
+        int a = money - 100;
+        int b = money - 50;
+        int c = money - 75;
+
+        if (a > 0) {
+            System.out.println("Would you like to buy something else?");
+        } else if (b > 0) {
+            System.out.println("Would you like to buy something else?");
+        } else if (c > 0) {
+            System.out.println("Would you like to buy something else?");
+        }
+    }
+}
